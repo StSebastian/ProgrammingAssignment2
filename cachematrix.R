6a10562ff3d803ccbbaa2687a81d8712c3cc0934
StSebastian <sebastian.st@mytum.de>
2014-05-22 10:31:33 +0200

    VersionOne

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function (y) {
+		x   <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(x) inv <<- solve(x) 
+	getinv <- function() inv
+	list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+	if(!is.null(inv))  {
+			message("getting cached data")
+			return(inv)
+	}
+	data <- x$get()
+	x$setinv(data)
+	x$getinv()
+	## Return a matrix that is the inverse of 'x'
 }
+
+a<-matrix(c(3,2,6,4),nrow=2)
+Fehler in solve.default(x) : 
+  System ist für den Rechner singulär: reziproke Konditionszahl = 7.40149e-18
+
